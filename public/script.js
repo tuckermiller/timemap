1eb3f8e0fca34a647a9573d30b380c03145afd8e
tuckermiller <tucker.miller@gmail.com>
2017-09-10 14:45:43 -0700

    Made Activity class

--- public/script.js
+++ public/script.js
@@ -1,30 +1,55 @@
-var interval;
-var timer = 0;
-var timerIsRunning = false;
+// Counter for timers, to give each one a unique identifier
+var timerIdCounter = 0;
+
+const TIMER_STATES = Object.freeze({
+    STOPPED: Symbol("STOPPED"),
+    RUNNING: Symbol("RUNNING")
+});
+
+class Activity {
+    constructor(name, timerId) {
+        this.name = name;
+        this.timerId = 'timer' + timerId;
+        this.timer = 0;
+        this.timerState = TIMER_STATES.STOPPED;
+    }
+
+    onClick() {
+        var self = this;
+        if (self.timerState === TIMER_STATES.STOPPED) {
+            self.timerState = TIMER_STATES.RUNNING;
+
+            self.interval = setInterval(function() {
+                self.timer += .1;
+                document.getElementById(self.timerId).innerText = parseFloat(self.timer).toFixed(1) + " seconds";
+            }, 100);
+        } else {
+            self.timerState = TIMER_STATES.STOPPED;
+            clearInterval(self.interval);
+        }
+    }
+}
 
 function addActivity() {
     // Get the value of the input field for activity
-    var activity = document.getElementById('activity-field').value;
+    var activityName = document.getElementById('activity-field').value;
 
-    // Render the new activity
+    // Create new activity
+    window[activityName] = new Activity(activityName, timerIdCounter++);
+
+    // Create activity button
     var li = document.createElement("li");
     var button = document.createElement("button");
-    button.onclick = activityClick;
-    button.appendChild(document.createTextNode(activity));
-    li.appendChild(button)
+    button.addEventListener("click", function() { window[activityName].onClick(); });
+    button.appendChild(document.createTextNode(window[activityName].name));
+
+    // Create timer
+    var span = document.createElement("span");
+    span.id = window[activityName].timerId;
+    span.innerText = " ";
+
+    // Render li
+    li.appendChild(button);
+    li.appendChild(span);
     document.getElementById('activities').appendChild(li);
-}
-
-function activityClick() {
-    if (!timerIsRunning) {
-        timerIsRunning = true;
-
-        interval = setInterval(function() {
-            timer += .1;
-            document.getElementById("timer-display").innerText = timer + " seconds";
-        }, 100);
-    } else {
-        timerIsRunning = false;
-        clearInterval(interval);
-    }
 }
